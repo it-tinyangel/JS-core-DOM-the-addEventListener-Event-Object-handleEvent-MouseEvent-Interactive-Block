938807a9ce7ad24690ceff1123d1b6c0cb055116
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2023-11-14 19:52:32 +0200

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 const box = document.getElementById('box');
-// Array of colors to change
 const colors = ['crimson', 'khaki', 'seagreen'];
+let colorIndex = 0;
 
-let i = 0;
-box.addEventListener('mouseover', function () {
-changeBackgroundColor(box, colors[i]);
-i = (i + 1) % colors.length;
-});
+// Event handler to responding the mouse hover
+box.addEventListener('mouseover', handleMouseOver);
 
-box.addEventListener('mouseout', function () {
-changeBackgroundColor(box, '');
-});
+// Event handler to the exit of the mouse
+box.addEventListener('mouseout', handleMouseOut);
 
-// Function to change the background color of the element
+// Function to change the background color element
 function changeBackgroundColor(element, color) {
-element.style.backgroundColor = color;
+	element.style.backgroundColor = color;
+}
+
+// Function to event handler 'mouseover'
+function handleMouseOver() {
+	changeBackgroundColor(box, colors[colorIndex]);
+	colorIndex = (colorIndex + 1) % colors.length;
+}
+
+// Function to event handler 'mouseout'
+function handleMouseOut() {
+	changeBackgroundColor(box, '');
 }
